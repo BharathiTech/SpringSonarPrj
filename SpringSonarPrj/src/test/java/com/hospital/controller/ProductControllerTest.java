c2b33e54f10dfb0c9711321ebd328ab1698b224b
Bharathi <bharathitech36@gmail.com>
2020-07-15 17:27:18 +0530

    Test cases modified

--- SpringSonarPrj/src/test/java/com/hospital/controller/ProductControllerTest.java
+++ SpringSonarPrj/src/test/java/com/hospital/controller/ProductControllerTest.java
@@ -1,43 +1,45 @@
 package com.hospital.controller;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import com.hospital.SpringSonarPrjApplicationTests;
-
-
-public class ProductControllerTest extends SpringSonarPrjApplicationTests{
+//@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ProductControllerTest{
+	@Test
+	void contextLoads() {
+	} 
 	
-	@Autowired
-	private WebApplicationContext webApplicationContext;
+	/*
+	 * @Autowired private WebApplicationContext webApplicationContext;
+	 */
 	
 	@Autowired
 	MockMvc mockMvc;
 	
-	@Before
-	public void setUp() {
-		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
-	}
+	/*
+	 * @Before public void setUp() { mockMvc =
+	 * MockMvcBuilders.webAppContextSetup(webApplicationContext).build(); }
+	 */
 
 	@Test
 	void testA() throws Exception {
-		mockMvc.perform(MockMvcRequestBuilders.get("product")).
+		mockMvc.perform(get("/product")).
 		andExpect(status().isOk()).
 		andExpect(content().contentType(MediaType.APPLICATION_JSON)).
 		andExpect(jsonPath("$.name").value("Brick"));

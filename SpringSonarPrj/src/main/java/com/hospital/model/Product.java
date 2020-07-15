744a76b80f64ae674bad044cd6fbcbbdb98507e7
Bharathi <bharathitech36@gmail.com>
2020-07-15 16:48:02 +0530

    product added

--- SpringSonarPrj/src/main/java/com/hospital/model/Product.java
+++ SpringSonarPrj/src/main/java/com/hospital/model/Product.java
@@ -9,5 +9,25 @@ public class Product {
 	private String p;
 	
 	
+	public BigInteger getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getP() {
+		return p;
+	}
+	public void setId(BigInteger id) {
+		this.id = id;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setP(String p) {
+		this.p = p;
+	}
+	
+	
 
 }
